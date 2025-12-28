e2676af7ebdc08a05a1c01e43f4a0b7ac2394681
Prathamesh Sawant <sprathamesh387@gmail.com>
2025-12-28 19:44:34 +0530

    02project

---
+++ 02_cards_project/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Card from "./components/Card";
+import "./index.css";
+
+
+function App() {
+  return (
+    <>
+      <div className="parent">
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
